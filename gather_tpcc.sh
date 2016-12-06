4c9dc525a9524bc05878c6d12f0a0660b7f0ec9f
eric-haibin-lin <linhaibin.eric@gmail.com>
2016-12-06 17:20:39 -0500

    Fix gather tpcc script

--- gather_tpcc.sh
+++ gather_tpcc.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
-for d in `ls | grep tpcc_collected | sort -t '_' -k10,10 -g`; do
-		b=`echo $d | cut -d'_' -f 10`
-	    echo -n -e "$b\t"
-done
-echo ""
-for d in `ls | grep tpcc_collected | sort -t '_' -k10,10 -g`; do
+
+for ratio in "44_44_4_4_4" "100_0_0_0_0" "0_0_100_0" "60_0_40_0" "10_0_90_0"; do
+	echo $ratio
+	for d in `ls | grep tpcc_collected | grep $ratio | sort -t '_' -k9,9 -g`; do
+		b=`echo $d | cut -d'_' -f 9`
+	    echo -n -e "$b "
+	done
+	echo ""
+	for d in `ls | grep tpcc_collected | grep $ratio | sort -t '_' -k9,9 -g`; do
 		#b=`echo $d | cut -d'-' -f 9`
 		thrpt=`head -6 ${d}/*.summary | tail -1`
-		echo -n -e "$thrpt\t"
+		echo -n -e "$thrpt "
+	done
+	echo ""
 done
-echo ""
